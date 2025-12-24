b5e224d0ddf129f1c672485e68d088dbca4d063f
david-why <david_why@outlook.com>
2025-12-24 18:21:01 +0800

    adjust seeker page styling

--- src/routes/seekers/+page.svelte
+++ src/routes/seekers/+page.svelte
@@ -22,13 +22,22 @@
 {#each game.spec.questions as question (question.id)}
   <details class="mb-2">
     <summary class="fs-3 fw-medium">{question.name}</summary>
-    <p>{question.question}</p>
+
+    <blockquote class="border-start border-secondary-subtle border-2 ps-2 mt-1 mb-0 fst-italic">
+      {question.question}
+    </blockquote>
+    <p class="my-2">Question cost: Draw {question.draw} keep {question.keep}</p>
     <div class="d-flex gap-2 flex-wrap mb-2">
       {#each question.options as option (option.id)}
         <Card style="width: 20em">
           <CardBody>
             <CardTitle>{option.text}</CardTitle>
-            <CardText>Asked {getAskCount(question.id, option.id)} times</CardText>
+            <CardText>
+              {#if option.description}
+                <p>{option.description}</p>
+              {/if}
+              <p>Asked {getAskCount(question.id, option.id)} times</p>
+            </CardText>
           </CardBody>
           <CardFooter>
             <Button size="sm" onclick={() => askQuestion(question.id, option.id)}>Ask</Button>
